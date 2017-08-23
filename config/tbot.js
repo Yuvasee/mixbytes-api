8e0817c74b9fbbb36c02b8d6edd6eb111781d8e6
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2017-08-24 01:15:38 +0300

    Initial commit

---
+++ config/tbot.js
@@ -0,0 +1,4 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.TELEGRAM_TOKEN;
+
+module.exports = new TelegramBot(token, {polling: true});
---
+++ server.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env nodejs
+require('dotenv').config();
+const MongoClient   = require('mongodb').MongoClient;
+const db            = require('./config/db');
+const app           = require('./config/express');
+const tBot          = require('./config/tbot');
+const port          = process.env.PORT;
+const tGroupId      = process.env.TELEGRAM_GROUP;
+
+MongoClient.connect(db.url, (err, db) => {
+  if (err) return console.log(err)
+
+  app.post('/message', (req, res) => {
+    db.collection('messages').insert(req.body, (err, result) => {
+      if (err) {
+        res.status(500).send({error: 'Ошибка отправки сообщения'});
+      } else {
+        res.send();
+      }
+    });
+
+    tBot.sendMessage(tGroupId, `Сообщение на сайте от ${req.body.contact}: ${req.body.message}`);
+  });
+
+  app.post('/email', (req, res) => {
+    db.collection('email').insert(req.body, (err, result) => {
+      if (err) {
+        res.status(500).send({error: 'Ошибка отправки сообщения'});
+      } else {
+        res.send();
+      }
+    });
+
+    tBot.sendMessage(tGroupId, `Подписался на новости о школе: ${req.body.address}`);
+  });
+
+  app.listen(port, () => {
+    console.log('We are live on port ' + port);
+  });
+
+})
